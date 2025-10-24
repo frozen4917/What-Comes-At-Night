3f829b4a5e73ed10683dd2a1da44dd3e46255903
frozen4917 <76933493+frozen4917@users.noreply.github.com>
2025-10-24 20:07:34 +0530

    Add eval option for debugging & player stats on prompt text

--- scripts/ui.js
+++ scripts/ui.js
@@ -5,8 +5,13 @@ import { loadGameData } from './loader.js';
 // WORK IN PROGRESS
 // BASIC UPDATE CONSOLE UI FUNCTION
 // Prints Text and actions. Handles option input.
-export function updateConsoleUI(promptText, actions, prompt) {
+export function updateConsoleUI(promptText, actions, prompt, gameState) {
     console.clear();
+    console.log(chalk.redBright(`HP: ${gameState.player.health}, Stamina: ${gameState.player.stamina}`));
+    console.log(chalk.yellowBright(``))
+    console.log(chalk.blueBright(`${gameState.world.currentPhaseId} - ${gameState.world.actionsRemaining} Actions remaining`));
+
+
     console.log(promptText);
     console.log('\n=================================\n');
 
@@ -24,7 +29,11 @@ export function updateConsoleUI(promptText, actions, prompt) {
         const choice = prompt('> ').toUpperCase();
         const choiceIndex = choice.charCodeAt(0) - 65; // A=0, B=1, C=2...
 
-        if (choiceIndex >= 0 && choiceIndex < actions.length) {
+        // IF Z, go to Debug mode. 
+        // WARNING: REMOVE AFTER COMPLETION
+        if (choiceIndex == 25) {
+            debugEval(prompt, gameState);
+        } else if (choiceIndex >= 0 && choiceIndex < actions.length) {
             // Valid choice
             return actions[choiceIndex];
         } else {
@@ -33,3 +42,16 @@ export function updateConsoleUI(promptText, actions, prompt) {
     }
 }
 
+function debugEval(prompt, gameState) {
+    while (true) {
+        try {
+            const exp = prompt('-> ');
+            if (exp === 'exit()') {
+                break;
+            }
+            console.log(eval(exp));
+        } catch (err) {
+            console.log(chalk.redBright("Error in expression!"));
+        }
+    }
+}
