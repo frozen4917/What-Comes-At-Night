80a3d74f21e09f91bbc24074063f955073c42348
frozen4917 <76933493+frozen4917@users.noreply.github.com>
2025-11-21 12:49:32 +0530

    Configure versioning and social meta tags

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import packageJson from './package.json' // <--- 1. Import this
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  // 2. Define the global constant here
+  define: {
+    '__APP_VERSION__': JSON.stringify(packageJson.version)
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
